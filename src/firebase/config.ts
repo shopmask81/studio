6614d6e31b7d15afa1d089fda5f7f4e934821a90
Mask Shop <shopmask81@gmail.com>
2025-11-11 17:54:50 +0000

    No, you are using the wrong firebase project and firestore, you are used

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:989649947313:web:bc9bb13c478ef81ac73090",
   "apiKey": "AIzaSyDJyF0PxzVFrypqaF_OFyi4sq6s6phagA8",
   "authDomain": "maskshop01.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "989649947313"
+  "measurementId": "G-5G3725T4M9",
+  "messagingSenderId": "989649947313",
+  "storageBucket": "maskshop01.appspot.com"
 };
