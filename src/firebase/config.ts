cd005e993577762baefe29c32ccfa2430d814b63
Mask Shop <shopmask81@gmail.com>
2025-11-11 15:20:34 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6890194652-5b37b",
+  "projectId": "maskshop01",
   "appId": "1:989649947313:web:bc9bb13c478ef81ac73090",
   "apiKey": "AIzaSyDJyF0PxzVFrypqaF_OFyi4sq6s6phagA8",
-  "authDomain": "studio-6890194652-5b37b.firebaseapp.com",
+  "authDomain": "maskshop01.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "989649947313"
 };
