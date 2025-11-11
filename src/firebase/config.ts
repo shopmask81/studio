04b238310318498576111da5670b703876d7a160
Mask Shop <shopmask81@gmail.com>
2025-11-11 15:06:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6890194652-5b37b",
+  "appId": "1:989649947313:web:bc9bb13c478ef81ac73090",
+  "apiKey": "AIzaSyDJyF0PxzVFrypqaF_OFyi4sq6s6phagA8",
+  "authDomain": "studio-6890194652-5b37b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "989649947313"
+};
